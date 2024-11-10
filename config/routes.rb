3ed8223d270b29b78501e9b6966a9e50c55dd6f8
Ege Demirci <egedemirci@sabanciuniv.edu>
2024-11-09 22:58:58 -0800

    Set routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/*
-  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:create, :show]
+      resources :profiles, only: [:show, :update]
+      resources :posts do
+        resources :comments, only: [:create, :index]
+      end
+      resources :messages, only: [:create, :index]
+      post 'login', to: 'sessions#create'
+      get 'logout', to: 'sessions#destroy'
+    end
+  end
 end
