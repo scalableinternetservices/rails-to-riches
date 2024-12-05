712bd6c787e6fffd910dd803a860d579bbcd4d27
praneeth-bala <praneethb2002@gmail.com>
2024-12-05 12:15:23 -0800

    fix photos workflow

--- scripts/pack_photo.py
+++ scripts/pack_photo.py
@@ -1,15 +1,13 @@
-prefix = b"""------WebKitFormBoundary7MA4YWxkTrZu0gW
-Content-Disposition: form-data; name="photo[image]"; filename="photo.webp"
-Content-Type: image/webp
+prefix = b"""\
+------WebKitFormBoundary7MA4YWxkTrZu0gW\r\n\
+Content-Disposition: form-data; name="photo[image]"; filename="photo.webp"\r\n\
+Content-Type: image/webp\r\n\r\n"""
 
-"""
-
-suffix=b"""
-------WebKitFormBoundary7MA4YWxkTrZu0gW
-Content-Disposition: form-data; name="photo[primary]"
-
-true
-------WebKitFormBoundary7MA4YWxkTrZu0gW--"""
+suffix = b"""\
+\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\n\
+Content-Disposition: form-data; name="photo[primary]"\r\n\r\n\
+true\r\n\
+------WebKitFormBoundary7MA4YWxkTrZu0gW--\r\n"""
 
 with open("photo_form.bin", "wb") as f:
     f.write(prefix)
